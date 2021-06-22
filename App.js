7f9a2e3411a92f7ce4ae23ec1406cc50b7694340
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-23 03:46:40 +0530

    Completed UI of the App

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        </View>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Task text={"This is task 1"} />
+          <Task text={"This is task 2"} />
+          <Task text={"This is task 3"} />
+          <Task text={"This is task 4"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +28,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d4d9d1",
+  },
+
+  tasksWrapper: {
+    paddingTop: 20,
+  },
+  titleContainer: {
+    backgroundColor: "#ebeaed",
+    width: "100%",
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+    paddingHorizontal: 20,
   },
 });
